6dd0c976eff5bf0a804a406e0e77b76baf11b0d0
Seveshy <danielalvesmajor@outlook.com>
2020-11-25 16:56:03 -0300

    First commit api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28f28c80659641d2592f07ad469a8aee'),
+    },
+  },
+});
